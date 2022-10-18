ce3056746db4b4e0a21b3240a0f1e6fbdc45e792
sevensquad7 <edgarxot@gmail.com>
2022-10-18 03:56:02 -0500

    Execution stack & timing execution

---
+++ 0x0C-Javascript_advanced/10-prime.js
@@ -0,0 +1,29 @@
+const isPrime = (number) => {
+    if (number < 2) return false;
+  
+    let sqrt = parseInt(Math.sqrt(number));
+  
+    for (let i = 2; i <= sqrt; i++) {
+      if (number % i === 0) return false;
+    }
+  
+    return true;
+  };
+  
+  const countPrimeNumbers = (number) => {
+    const array = [2];
+  
+    for (let i = 3; i < number; i += 2) {
+      if (isPrime(i)) array.push(i);
+    }
+  
+    return array.length;
+  };
+  
+  let t0 = performance.now();
+  for (let i = 0; i < 100; i++) countPrimeNumbers(100);
+  let t1 = performance.now();
+  
+  console.log(
+    `Execution time of printing countPrimeNumbers was ${t1 - t0} milliseconds.`
+  );
